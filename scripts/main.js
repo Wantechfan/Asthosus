a9468ca8c156150ffe73331c9c309962208d2629
WantechFan <alifiandratsaqifp@gmail.com>
2026-06-05 14:18:54 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,5 @@ asthosus.displayName = Core.bundle.get("mod.asthosus.name");
 asthosus.author = Core.bundle.get("mod.asthosus.author");
 asthosus.description = Core.bundle.get("mod.asthosus.description");
 asthosus.subtitle = Core.bundle.get("mod.asthosus.subtitle");
+
+//If I accidentally submitted this on pr, replace .js with .txt
